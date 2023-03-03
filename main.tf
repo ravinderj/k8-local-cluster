c1d2ae1005fdf351a2b1123cf4017dcaa0a918ed
Ravinder Jajoria <jajoriaravinder99@gmail.com>
2023-03-03 09:31:28 +0530

    Create local container registry

--- main.tf
+++ main.tf
@@ -9,10 +9,19 @@ module "cluster" {
   cluster_name = local.environments[count.index]
 }
 
-
 resource "local_sensitive_file" "kubeconfig" {
   count = length(local.environments)
 
   content  = module.cluster[count.index].kubeconfig
   filename = pathexpand("~/.k3d/kubeconfig-${local.environments[count.index]}.yaml")
+}
+
+# Create local container registry
+resource "k3d_registry" "myregistry" {
+    name = "myregistry.local"
+    port {
+      host = "localhost"
+      host_ip = "0.0.0.0"
+      host_port = "5002"
+    }
 }
--- modules/local/cluster.tf
+++ modules/local/cluster.tf
@@ -2,6 +2,12 @@ resource "k3d_cluster" "swarm" {
   name    = var.cluster_name
   servers = 1
   agents  = 2
+
+  registries {
+    use = [
+      "k3d-myregistry.local:5002"
+    ]
+  }
 }
 
 data "k3d_cluster" "swarm" {
